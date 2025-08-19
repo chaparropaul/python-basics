9605423a7948681268f379328b5692dc642a752c
Paul Chaparro <paulchaparro212@gmail.com>
2025-08-19 15:36:58 -0500

    First Python script: hello stranger

---
+++ hello.py
@@ -0,0 +1,12 @@
+import time
+
+print("Hello Stranger!")
+
+time.sleep(2)
+
+name = input("What is your name?")
+
+age = int(input("How old are you?"))
+
+print("Nice to meet you, "+str(age)+" year old "+name+"!")
+
